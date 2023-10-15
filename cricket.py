b51df6114f6ec5f7c3280fb02da935f54dd40ab2
Imam Shaik <134620848+Imamsk13@users.noreply.github.com>
2023-10-15 11:53:20 +0530

    Add files via upload

--- cricket.py
+++ cricket.py
@@ -7,11 +7,11 @@ def play_cricket():
     for over in range(1, 6):  # 5 overs per inning
         print(f"Over {over} - User's Turn")
         for ball in range(1, 7):
-            user_choice = input(f"Ball {ball}: Enter 'hit' to hit the ball: ").strip().lower()
+            user_choice = input(f"Ball {ball}: Enter 'h' to hit the ball: ").strip().lower()
             ai_bowler_actions = {"out": 0, "dot": 0, "1": 1, "2": 2, "4": 4, "6": 6}
             ai_bowler_action = random.choice(list(ai_bowler_actions.keys()))
             
-            if user_choice == "hit":
+            if user_choice == "h":
                 if ai_bowler_action == "out":
                     print("You're out!")
                     user_wickets += 1
@@ -29,13 +29,26 @@ def play_cricket():
     ai_wickets = 0
 
     for over in range(1, 6):
+        print(f"Over {over} - User's Turn")
         for ball in range(1, 7):
-            ai_bowler_actions = {"dot": 0, "1": 1, "2": 2, "4": 4, "6": 6}
-            ai_bowler_action = random.choice(list(ai_bowler_actions.keys()))
-            runs = ai_bowler_actions[ai_bowler_action]
-            ai_score += runs
+            user_choice = input(f"Ball {ball}: Enter 'b' to bowl: ").strip().lower()
+            ai_batter_actions = {"out": 0, "dot": 0, "1": 1, "2": 2, "4": 4, "6": 6}
+            ai_batter_action = random.choice(list(ai_batter_actions.keys()))
+            
+            if user_choice == "b":
+                if ai_batter_action == "out":
+                    print("wicket!")
+                    ai_wickets += 1
+                else:
+                    runs = ai_batter_actions[ai_batter_action]
+                    ai_score += runs
+                    print(f"AT scored {runs} runs!")
+            else:
+                ai_score += 6
+                print("its a six.")
 
-    print(f"AI's Score after 5 overs: {ai_score}/{ai_wickets}")
+        print(f"AI Score after over {over}: {ai_score}/{ai_wickets}")
+
 
     print("Match Over")
     if user_score > ai_score:
